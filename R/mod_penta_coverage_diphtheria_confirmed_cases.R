9a209c057b6229f7708478d52a71ddeef3bb0d03
Amanyiraho <amanyiraho@gmail.com>
2024-01-22 04:39:29 +0300

    add new db connection

--- R/mod_penta_coverage_diphtheria_confirmed_cases.R
+++ R/mod_penta_coverage_diphtheria_confirmed_cases.R
@@ -67,43 +67,50 @@ mod_penta_coverage_diphtheria_confirmed_cases_server <- function(id,
                across(c(Year,State ), as.factor))
     })
 
+    # chart_data <-  dplyr::tbl(connection, "diphtheria_alt_denominator")%>%
+    #   dplyr::filter(Year %in% !! "2023"&
+    #                   Months %in% !!"Aug" &
+    #                   State %in% !!"Federal Capital Territory" &
+    #                   LGA %in% !!"State level data")%>% collect() %>%
+    #   mutate(Months = as.Date(str_c(Year, Months, 01,sep = "-"), "%Y-%b-%d"),
+    #          across(c(Year,State ), as.factor))
 
 
     indicator_plot <- reactive({
 
-      min_max_rate <- range(chart_data()$`MCV 1 Alt Denominator`,na.rm = T)
+      min_max_rate <- range(chart_data()$`first dose coverage alt denominator`,na.rm = T)
 
-      min_max_number <-  range(chart_data()$`Measles Cases (CaseBased)`,na.rm = T)
+      min_max_number <-  range(chart_data()$`Cases (CaseBased)`,na.rm = T)
 
 
       plotmcac <- plot_ly(data = chart_data() %>% arrange(Months))
 
       plotmcac <- plotmcac %>% add_trace(x = ~Months,
-                                         y = ~`MCV 1`,
+                                         y = ~`first dose coverage`,
                                          yaxis = 'y2',
                                          color = I("#004e64"),
                                          mode = 'lines+markers', type = 'scatter',
                                          line = list(shape = 'spline', linetype = I("solid")),
                                          marker = list(symbol = I("circle")),
-                                         name = 'MCV 1(DHIS2)',
+                                         name = 'Penta 1(DHIS2)',
                                          hovertemplate = paste('<b>Coverage %</b>: %{y:.1f}',
                                                                '<br><b style="text-align:left;">Month </b>: %{x}<br>')
       )
 
       plotmcac <- plotmcac %>% add_trace(x = ~Months,
-                                         y = ~`MCV 2`,
+                                         y = ~`later dose coverage`,
                                          yaxis = 'y2',
                                          color = I("#94D2BD"),
                                          mode = 'lines+markers', type = 'scatter',
                                          line = list(shape = 'spline', linetype = I("solid")),
                                          marker = list(symbol = I("circle")),
-                                         name = 'MCV 2(DHIS2)',
+                                         name = 'Penta 3(DHIS2)',
                                          hovertemplate = paste('<b>Coverage %</b>: %{y:.1f}',
                                                                '<br><b style="text-align:left;">Month </b>: %{x}<br>')
       )
 
       plotmcac <- plotmcac %>% add_trace(x = ~ Months,
-                                         y = ~ `MCV 1 Alt Denominator`,
+                                         y = ~ `first dose coverage alt denominator`,
                                          yaxis = 'y2',
                                          color = I("#edb952"),
                                          line = list(shape = 'spline', linetype = I("solid")),
@@ -111,10 +118,10 @@ mod_penta_coverage_diphtheria_confirmed_cases_server <- function(id,
                                          mode = 'lines+markers', type = 'scatter',
                                          hovertemplate = paste('<b>Coverage %</b>: %{y:.1f}',
                                                                '<br><b style="text-align:left;">Month </b>: %{x}<br>'),
-                                         name = 'MCV 1 Alt Denominator')
+                                         name = 'Penta 1 Alt Denominator')
 
       plotmcac <- plotmcac %>% add_trace(x = ~ Months,
-                                         y = ~ `MCV 2 Alt Denominator`,
+                                         y = ~ `later dose coverage alt denominator`,
                                          yaxis = 'y2',
                                          color = I("#E9D8A6"),
                                          line = list(shape = 'spline', linetype = I("solid")),
@@ -122,13 +129,13 @@ mod_penta_coverage_diphtheria_confirmed_cases_server <- function(id,
                                          mode = 'lines+markers', type = 'scatter',
                                          hovertemplate = paste('<b>Coverage %</b>: %{y:.1f}',
                                                                '<br><b style="text-align:left;">Month </b>: %{x}<br>'),
-                                         name = 'MCV 2 Alt Denominator')
+                                         name = 'Penta 3 Alt Denominator')
 
       plotmcac <- plotmcac %>% add_trace(x = ~ Months,
-                                         y = ~ `Measles Cases (CaseBased)`,
+                                         y = ~ `Cases (CaseBased)`,
                                          type = 'bar',
                                          color =  I("#00a5cf"),
-                                         name = 'Measles Cases (Sormas)',
+                                         name = 'Diphtheria Cases (Sormas)',
                                          #   marker = list(color = '#0000ff'),
                                          hovertemplate = paste('<b>Cases</b>: %{y:.0f}',
                                                                '<br><b style="text-align:left;">Month </b>: %{x}<br>')
@@ -199,7 +206,7 @@ mod_penta_coverage_diphtheria_confirmed_cases_server <- function(id,
     output$downloadData <- downloadHandler(
 
       filename = function() {
-        paste0("Chart 1- Measles",  picker_state_var(), picker_lga_var(),".csv")
+        paste0("Chart 1- Penta",  picker_state_var(), picker_lga_var(),".csv")
       },
       content = function(file) {
 
@@ -210,7 +217,7 @@ mod_penta_coverage_diphtheria_confirmed_cases_server <- function(id,
 
     output$downloadChart <- downloadHandler(
       filename = function() {
-        paste0("Chart 1-  Measles", picker_state_var(), picker_lga_var(),".png")
+        paste0("Chart 1-  Penta", picker_state_var(), picker_lga_var(),".png")
       },
       content = function(file) {
         owd <- setwd(tempdir())
